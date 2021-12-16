d355a8fe670901a0a99e35bdce9cc65495a68474
FIANERD <mattiasson.sofia@gmail.com>
2021-12-16 22:59:36 +0100

    Add dbcontext

---
+++ FreakyFashionServices.CatalogService/Data/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using FreakyFashionServices.CatalogService.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashionServices.CatalogService.Data
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+           
+        }
+    }
+}
