797a96e511af003b00f30cf0d9356ce5c13f12e3
FIANERD <mattiasson.sofia@gmail.com>
2021-12-16 22:22:08 +0100

    Add catalog dto

---
+++ FreakyFashionServices.CatalogService/Controllers/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace FreakyFashionServices.CatalogService.Controllers
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+        public string UrlSlug { get; set; }
+    }
+}
---
+++ FreakyFashionServices.CatalogService/Models/Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace FreakyFashionServices.CatalogService.Models.Domain
+{
+    public class Product
+    {
+        public Product(string name, string description, string imageUrl, int price)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+    }
+}
