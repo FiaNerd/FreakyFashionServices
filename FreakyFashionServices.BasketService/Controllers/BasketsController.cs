1a696bc71455651e2b82d47580516a73dd0ac7b0
FIANERD <mattiasson.sofia@gmail.com>
2021-12-19 22:12:30 +0100

    Add Put controller to basket

---
+++ FreakyFashionServices.BasketService/Controllers/BasketsController.cs
@@ -0,0 +1,23 @@
+﻿using FreakyFashionServices.BasketService.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreakyFashionServices.BasketService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        //public BasketsController(IDistributedCache cache)
+        //{
+        //    Cache = cache;
+        //}
+
+        //public IDistributedCache? Cache { get; }
+
+        [HttpPut]
+        public IActionResult CreateReplaceBasket(BasketDto basketDto)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ FreakyFashionServices.BasketService/Models/Domain/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreakyFashionServices.BasketService.Models.Domain
+{
+    public class Basket
+    {
+        public Basket(int customerNumber, string productName, int price, int quantity)
+        {
+            CustomerNumber = customerNumber;
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        [Key]
+        public int CustomerNumber { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
