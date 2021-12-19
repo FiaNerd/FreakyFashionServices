ad88874cca68acbe472fb5278f9376b6b9922d71
FIANERD <mattiasson.sofia@gmail.com>
2021-12-19 22:57:46 +0100

    Add jsons Seralized to BasketController

--- FreakyFashionServices.BasketService/Models/Dto/BasketDto.cs
+++ FreakyFashionServices.BasketService/Models/Dto/BasketDto.cs
@@ -2,7 +2,7 @@
 {
     public class BasketDto
     {
-        public int CustomerNumber { get; set; }
+        public string CustomerNumber { get; set; }
         public string ProductName { get; set; }
         public int Price { get; set; }
         public int Quantity { get; set; }
