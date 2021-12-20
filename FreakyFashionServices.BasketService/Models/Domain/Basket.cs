2b77a1f7e804c4b151f05cd181e3b8fe4d4aaaca
FIANERD <mattiasson.sofia@gmail.com>
2021-12-21 00:14:01 +0100

    Add put an get in redis finished

--- FreakyFashionServices.BasketService/Models/Domain/Basket.cs
+++ FreakyFashionServices.BasketService/Models/Domain/Basket.cs
@@ -4,16 +4,17 @@ namespace FreakyFashionServices.BasketService.Models.Domain
 {
     public class Basket
     {
-        public Basket(int orderNumber, int productId, int quantity)
-        {
-            OrderNumber = orderNumber;
-            ProductId = productId;
-            Quantity = quantity;
-        }
+        //public Basket(int orderNumber, int productId, int quantity)
+        //{
+        //    OrderNumber = orderNumber;
+        //    ProductId = productId;
+        //    Quantity = quantity;
+        //}
 
         [Key]
         public int OrderNumber { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
+
+        public List<Items> Items { get; set; }
+
     }
 }
--- FreakyFashionServices.BasketService/Models/Dto/BasketDto.cs
+++ FreakyFashionServices.BasketService/Models/Dto/BasketDto.cs
@@ -1,9 +1,13 @@
-﻿namespace FreakyFashionServices.BasketService.Models.Dto
+﻿using FreakyFashionServices.BasketService.Models.Domain;
+
+namespace FreakyFashionServices.BasketService.Models.Dto
 {
     public class BasketDto
     {
         public int OrderNumber { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        //public int ProductId { get; set; }
+        //public int Quantity { get; set; }
+
+        public List<Items> Items { get; set; } = new List<Items>();
     }
 }
